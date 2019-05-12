707233ae1de74b24a77dd77429e683aa936853db
rxhack <50539435+rxhack@users.noreply.github.com>
2019-05-13 01:03:24 +0530

    auth change

--- routes/auth.js
+++ routes/auth.js
@@ -3,17 +3,25 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('respond with a resource',{
+    title : "Login"
+  });
 });
 router.get('/authorg/login',function(req,res,next){
-  res.render('auth/organization/login');
+  res.render('auth/organization/login',{
+    title : "Login"
+  });
 });
 
 router.get('/authorg/signup',function(req,res,next){
-res.render('auth/organization/signup');
+res.render('auth/organization/signup',{
+    title : "Organization signup"
+  });
 });
 
 router.get('/authorg/setup/profile',function(req,res,next){
-res.render('auth/organization/profileSetup');
+res.render('auth/organization/profileSetup',{
+    title : "Organization profile setup"
+  });
 });
 module.exports = router;
